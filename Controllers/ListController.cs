7ca5082d4be9b6e4454908490d9015d6b2d07f0d
superivaj <pareja.javier@gmail.com>
2021-10-15 10:18:09 +0200

    Initial commit

---
+++ Controllers/ListController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InterviewTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ListController : ControllerBase
+    {
+        private readonly ILogger<ListController> logger;
+
+        public ListController(ILogger<ListController> logger)
+        {
+            this.logger = logger;
+        }
+
+        /*
+         * List API methods goe here
+         * */
+    }
+}
